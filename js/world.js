4246ab0d7647230e5cec9117c2841445d925d93e
stubsmalone1982-source <stubsmalone1982@gmail.com>
2026-02-26 11:16:51 -0600

    Update world.js

--- js/world.js
+++ js/world.js
@@ -52,18 +52,27 @@ function loadTexture(url) {
 
   const image = new Image();
   image.src = url;
-  image.onload = () => {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      0,
-      gl.RGBA,
-      gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      image
-    );
+image.onload = () => {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    image
+  );
+
+  const isPowerOf2 = (value) => (value & (value - 1)) === 0;
+
+  if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
     gl.generateMipmap(gl.TEXTURE_2D);
-  };
+  } else {
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  }
+}; Fair enough
 
   return texture;
 }
@@ -270,4 +279,5 @@ function loop(now) {
 }
 
 requestAnimationFrame(loop);
+
 
